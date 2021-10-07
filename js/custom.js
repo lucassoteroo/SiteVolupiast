08c573296dca884f60441ffb0279c23c0dcce360
lucassoteroo <lucassoteroo14@gmail.com>
2021-10-07 17:35:11 -0300

    melhorando evento

--- js/custom.js
+++ js/custom.js
@@ -2,9 +2,12 @@ $(document).ready(function() {
     $('.nav-modal-open').on('click', function(event) {
         event.preventDefault();
         let elemento = $(this).attr('rel');
-        $('.modal-body').html($('#' + elemento).html());
+        $('#modelId .modal-body').html($('#' + elemento).html());
         let myModal = new bootstrap.Modal($('#modelId'));
         myModal.show();
-        $('.modal-header h5.modal-title').html($(this).text());
+        $('#modelId .modal-header h5.modal-title').html($(this).text());
+        $('.btn-close').on('click', function() {
+            let elemento = '';
+        })
     });
 });
