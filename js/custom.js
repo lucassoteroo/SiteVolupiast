c98adfe8c2c611998467bf5577678dfd8b41df43
lucassoteroo <lucassoteroo14@gmail.com>
2021-10-13 21:46:23 -0300

    adicionando produtos no campo 'carrinho'

--- js/custom.js
+++ js/custom.js
@@ -17,63 +17,63 @@ $(document).ready(function() {
         if (elemento == 'Camisa Volúpia') {
             let preco = 79.90;
             let tamanhos = ["P", "M", "G"];
-            console.log("Nome: " + elemento);
-            console.log("Preço: " + preco + " R$");
-            console.log("Tamanhos: " + tamanhos);
+            $("#nome").append("Produto: " + elemento);
+            $("#preco").append("Preço: " + preco + " R$");
+            $("#tamanho").append("Tamanho: " + tamanhos);
         } else if (elemento == "Blusa Me") {
             let preco = 29.90;
             let tamanhos = ["P", "M", "G"];
-            console.log("Nome: " + elemento);
-            console.log("Preço: " + preco + " R$");
-            console.log("Tamanhos: " + tamanhos);
+            $("#nome").append("Produto: " + elemento);
+            $("#preco").append("Preço: " + preco + " R$");
+            $("#tamanho").append("Tamanho: " + tamanhos);
         } else if (elemento == "Calça Cargo") {
             let preco = 99.90;
             let tamanhos = ["P", "M", "G"];
-            console.log("Nome: " + elemento);
-            console.log("Preço: " + preco + " R$");
-            console.log("Tamanhos: " + tamanhos);
+            $("#nome").append("Produto: " + elemento);
+            $("#preco").append("Preço: " + preco + " R$");
+            $("#tamanho").append("Tamanho: " + tamanhos);
         } else if (elemento == "Short Malu") {
             let preco = 49.90;
             let tamanhos = ["P", "M", "G"];
-            console.log("Nome: " + elemento);
-            console.log("Preço: " + preco + " R$");
-            console.log("Tamanhos: " + tamanhos);
+            $("#nome").append("Produto: " + elemento);
+            $("#preco").append("Preço: " + preco + " R$");
+            $("#tamanho").append("Tamanho: " + tamanhos);
         } else if (elemento == "Cropped Angry") {
             let preco = 29.90;
             let tamanhos = ["P", "M", "G"];
-            console.log("Nome: " + elemento);
-            console.log("Preço: " + preco + " R$");
-            console.log("Tamanhos: " + tamanhos);
+            $("#nome").append("Produto: " + elemento);
+            $("#preco").append("Preço: " + preco + " R$");
+            $("#tamanho").append("Tamanho: " + tamanhos);
         } else if (elemento == "Top Mazi") {
             let preco = 29.90;
             let tamanhos = ["P"];
-            console.log("Nome: " + elemento);
-            console.log("Preço: " + preco + " R$");
-            console.log("Tamanhos: " + tamanhos);
+            $("#nome").append("Produto: " + elemento);
+            $("#preco").append("Preço: " + preco + " R$");
+            $("#tamanho").append("Tamanho: " + tamanhos);
         } else if (elemento == "Top Gi") {
             let preco = 39.90;
             let tamanhos = ["P", "M"];
-            console.log("Nome: " + elemento);
-            console.log("Preço: " + preco + " R$");
-            console.log("Tamanhos: " + tamanhos);
+            $("#nome").append("Produto: " + elemento);
+            $("#preco").append("Preço: " + preco + " R$");
+            $("#tamanho").append("Tamanho: " + tamanhos);
         } else if (elemento == "Top Malu") {
             let preco = 39.90;
             let tamanhos = ["P", "M", "G"];
-            console.log("Nome: " + elemento);
-            console.log("Preço: " + preco + " R$");
-            console.log("Tamanhos: " + tamanhos);
+            $("#nome").append("Produto: " + elemento);
+            $("#preco").append("Preço: " + preco + " R$");
+            $("#tamanho").append("Tamanho: " + tamanhos);
         } else if (elemento == "Conjunto Gi") {
             let preco = 79.90;
             let tamanhos = ["P", "M"];
-            console.log("Nome: " + elemento);
-            console.log("Preço: " + preco + " R$");
-            console.log("Tamanhos: " + tamanhos);
+            $("#nome").append("Produto: " + elemento);
+            $("#preco").append("Preço: " + preco + " R$");
+            $("#tamanho").append("Tamanho: " + tamanhos);
         } else if (elemento == "Vestido Soft") {
             let preco = 89.90;
             let tamanhos = ["P", "M", "G"];
-            console.log("Nome: " + elemento);
-            console.log("Preço: " + preco + " R$");
-            console.log("Tamanhos: " + tamanhos);
+            $("#nome").append("Produto: " + elemento);
+            $("#preco").append("Preço: " + preco + " R$");
+            $("#tamanho").append("Tamanho: " + tamanhos);
         } else {
             alert("Produto não encontrado");
         }
