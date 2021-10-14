2dcc840384e26ec676e9ae6216755e6cd7a433fa
lucassoteroo <lucassoteroo14@gmail.com>
2021-10-13 21:15:39 -0300

    capturando valores com JQuery

--- js/custom.js
+++ js/custom.js
@@ -13,13 +13,69 @@ $(document).ready(function() {
 
     $('.btn-warning').on('click', function(event) {
         event.preventDefault();
-        let nomeId = $('.modal-title').text();
-        $('.btn-close').on('click', function(event) {
-            let nomeId = '';
-        })
-        console.log(nomeId);
-
-        // let nome = $('#nome').text();
-        // console.log(nome);
+        let elemento = $(this).attr('rel');
+        if (elemento == 'Camisa Volúpia') {
+            let preco = 79.90;
+            let tamanhos = ["P", "M", "G"];
+            console.log("Nome: " + elemento);
+            console.log("Preço: " + preco + " R$");
+            console.log("Tamanhos: " + tamanhos);
+        } else if (elemento == "Blusa Me") {
+            let preco = 29.90;
+            let tamanhos = ["P", "M", "G"];
+            console.log("Nome: " + elemento);
+            console.log("Preço: " + preco + " R$");
+            console.log("Tamanhos: " + tamanhos);
+        } else if (elemento == "Calça Cargo") {
+            let preco = 99.90;
+            let tamanhos = ["P", "M", "G"];
+            console.log("Nome: " + elemento);
+            console.log("Preço: " + preco + " R$");
+            console.log("Tamanhos: " + tamanhos);
+        } else if (elemento == "Short Malu") {
+            let preco = 49.90;
+            let tamanhos = ["P", "M", "G"];
+            console.log("Nome: " + elemento);
+            console.log("Preço: " + preco + " R$");
+            console.log("Tamanhos: " + tamanhos);
+        } else if (elemento == "Cropped Angry") {
+            let preco = 29.90;
+            let tamanhos = ["P", "M", "G"];
+            console.log("Nome: " + elemento);
+            console.log("Preço: " + preco + " R$");
+            console.log("Tamanhos: " + tamanhos);
+        } else if (elemento == "Top Mazi") {
+            let preco = 29.90;
+            let tamanhos = ["P"];
+            console.log("Nome: " + elemento);
+            console.log("Preço: " + preco + " R$");
+            console.log("Tamanhos: " + tamanhos);
+        } else if (elemento == "Top Gi") {
+            let preco = 39.90;
+            let tamanhos = ["P", "M"];
+            console.log("Nome: " + elemento);
+            console.log("Preço: " + preco + " R$");
+            console.log("Tamanhos: " + tamanhos);
+        } else if (elemento == "Top Malu") {
+            let preco = 39.90;
+            let tamanhos = ["P", "M", "G"];
+            console.log("Nome: " + elemento);
+            console.log("Preço: " + preco + " R$");
+            console.log("Tamanhos: " + tamanhos);
+        } else if (elemento == "Conjunto Gi") {
+            let preco = 79.90;
+            let tamanhos = ["P", "M"];
+            console.log("Nome: " + elemento);
+            console.log("Preço: " + preco + " R$");
+            console.log("Tamanhos: " + tamanhos);
+        } else if (elemento == "Vestido Soft") {
+            let preco = 89.90;
+            let tamanhos = ["P", "M", "G"];
+            console.log("Nome: " + elemento);
+            console.log("Preço: " + preco + " R$");
+            console.log("Tamanhos: " + tamanhos);
+        } else {
+            alert("Produto não encontrado");
+        }
     })
 });
