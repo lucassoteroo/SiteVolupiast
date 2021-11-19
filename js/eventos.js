51a594b0b5159b9ede70394c67f04dc8a8ae71d5
lucassoteroo <lucassoteroo14@gmail.com>
2021-11-18 22:11:20 -0300

    criando eventos do carrinho 3

--- js/eventos.js
+++ js/eventos.js
@@ -1,125 +1,83 @@
 // eventos carrinho
-
-var roupas = [
-    camisaVolupia = {
-        nome: 'Camisa Volúpia',
-        preço: 79.90
-    },
-    blusaMe = {
-        nome: 'Blusa Me',
-        preço: 29.90
-    },
-    calcaCargo = {
-        nome: 'Calça Cargo',
-        preço: 99.90
-    },
-    shortMalu = {
-        nome: 'Short Malu',
-        preço: 49.90
-    },
-    croppedAngry = {
-        nome: 'Cropped Angry',
-        preço: 29.90
-    },
-    topMazi = {
-        nome: 'Top Mazi',
-        preço: 59.90
-    },
-    topGi = {
-        nome: 'Top Gi',
-        preço: 39.90
-    },
-    topMalu = {
-        nome: 'Top Malu',
-        preço: 39.90
-    },
-    conjGi = {
-        nome: 'Conjunto Gi',
-        preço: 79.90
-    },
-    vestidoSoft = {
-        nome: 'Vestido Soft',
-        preço: 89.90
-    }
-]
-
+var roupas = []
 
 $(document).ready(function() {
     $('.btn-warning').on('click', function(event) {
         event.preventDefault();
 
-        function camisaVolupia() {
-            $('#produto').html(roupas[0].nome);
-            $('#preco').html(roupas[0].preço);
+        camisaVolupia = {
+            nome: 'Camisa Volúpia',
+            preço: 79.90
         }
 
-        function blusaMe() {
-            $('#produto').html(roupas[1].nome);
-            $('#preco').html(roupas[1].preço);
+        blusaMe = {
+            nome: 'Blusa Me',
+            preço: 29.90
         }
 
-        function calcaCargo() {
-            $('#produto').html(roupas[2].nome);
-            $('#preco').html(roupas[2].preço);
+        calcaCargo = {
+            nome: 'Calça Cargo',
+            preço: 99.90
         }
 
-        function shortMalu() {
-            $('#produto').html(roupas[3].nome);
-            $('#preco').html(roupas[3].preço);
+        shortMalu = {
+            nome: 'Short Malu',
+            preço: 49.90
         }
 
-        function croppedAngry() {
-            $('#produto').html(roupas[4].nome);
-            $('#preco').html(roupas[4].preço);
+        croppedAngry = {
+            nome: 'Cropped Angry',
+            preço: 29.90
         }
 
-        function topMazi() {
-            $('#produto').html(roupas[5].nome);
-            $('#preco').html(roupas[5].preço);
+        topMazi = {
+            nome: 'Top Mazi',
+            preço: 59.90
         }
 
-        function topGi() {
-            $('#produto').html(roupas[6].nome);
-            $('#preco').html(roupas[6].preço);
+        topGi = {
+            nome: 'Top Gi',
+            preço: 39.90
         }
 
-        function topMalu() {
-            $('#produto').html(roupas[7].nome);
-            $('#preco').html(roupas[7].preço);
+        topMalu = {
+            nome: 'Top Malu',
+            preço: 39.90
         }
 
-        function conjGi() {
-            $('#produto').html(roupas[8].nome);
-            $('#preco').html(roupas[8].preço);
+        conjGi = {
+            nome: 'Conjunto Gi',
+            preço: 79.90
         }
 
-        function vestidoSoft() {
-            $('#produto').html(roupas[9].nome);
-            $('#preco').html(roupas[9].preço);
+        vestidoSoft = {
+            nome: 'Vestido Soft',
+            preço: 89.90
         }
 
         let e = $(this).attr('rel');
         if (e == 1) {
-            camisaVolupia();
+            roupas.push(camisaVolupia);
+            console.log(roupas)
         } else if (e == 2) {
-            blusaMe();
+            roupas.push(blusaMe);
+            console.log(roupas)
         } else if (e == 3) {
-            calcaCargo();
+            roupas.push(calcaCargo);
         } else if (e == 4) {
-            shortMalu();
+            roupas.push(shortMalu);
         } else if (e == 5) {
-            croppedAngry();
+            roupas.push(croppedAngry);
         } else if (e == 6) {
-            topMazi();
+            roupas.push(topMazi);
         } else if (e == 7) {
-            topGi();
+            roupas.push(topGi);
         } else if (e == 8) {
-            topMalu();
+            roupas.push(topMalu);
         } else if (e == 9) {
-            conjGi();
+            roupas.push(conjGi);
         } else if (e == 10) {
-            vestidoSoft();
+            roupas.push(vestidoSoft);
         }
-
     })
 })
